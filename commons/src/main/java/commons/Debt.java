2815a7c3799914ddd7a102d0782559646f280d07
Lara Glamuzina <l.glamuzina@student.tudelft.nl>
2024-02-23 13:50:34 +0100

    improving/finishing the Debt class

--- commons/src/main/java/commons/Debt.java
+++ commons/src/main/java/commons/Debt.java
@@ -11,27 +11,27 @@ public class Debt {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private int amount;
-    private Participant personOwned;
+    private Participant personOwed;
     private Participant personInDebt;
-    private boolean payedOf;
+    private boolean paidOff;
     private Date dateCreation;
     private Expense debtFor;
 
     /**
      * constructs a participants debt related to a specific expense
      * @param amount amount owed in debt
-     * @param personOwned the person who needs to be paid to
+     * @param personOwed the person who needs to be paid to
      * @param personInDebt person who needs to pay the debt
-     * @param payedOf states if the debts been paid
+     * @param paidOff states if the debts been paid
      * @param dateCreation date when the debt was created
      * @param debtFor which expense is the debt for
      */
-    public Debt(int amount, Participant personOwned, Participant personInDebt,
-                boolean payedOf, Date dateCreation, Expense debtFor) {
+    public Debt(int amount, Participant personOwed, Participant personInDebt,
+                boolean paidOff, Date dateCreation, Expense debtFor) {
         this.amount = amount;
-        this.personOwned = personOwned;
+        this.personOwed = personOwed;
         this.personInDebt = personInDebt;
-        this.payedOf = payedOf;
+        this.paidOff = paidOff;
         this.dateCreation = dateCreation;
         this.debtFor = debtFor;
     }
@@ -59,79 +59,140 @@ public class Debt {
         return amount;
     }
 
-    public Participant getPersonOwned() {
-        return personOwned;
+    /**
+     * retrieves the participant that is owed the money
+     * @return the participant
+     */
+    public Participant getPersonOwed() {
+        return personOwed;
     }
 
+    /**
+     * retrieves the participant in debt
+     * @return participant in debt
+     */
     public Participant getPersonInDebt() {
         return personInDebt;
     }
 
-    public boolean isPayedOf() {
-        return payedOf;
+    /**
+     * retirieves the boolean value is the debt paid of
+     * @return boolean isPaidOf
+     */
+    public boolean isPaidOff() {
+        return paidOff;
     }
 
+    /**
+     * retrieves the fate the debt was created
+     * @return date of creation
+     */
     public Date getDateCreation() {
         return dateCreation;
     }
 
+    /**
+     * retrieves the expense the debt is related to
+     * @return the expense
+     */
     public Expense getDebtFor() {
         return debtFor;
     }
 
+    /**
+     * sets the id of the debt
+     * @param id the id of the debt to set
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * sets the amount of the debt
+     * @param amount the amount of the debt to set
+     */
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
-    public void setPersonOwned(Participant personOwned) {
-        this.personOwned = personOwned;
+    /**
+     * setes the person owed the debt
+     * @param personOwed person owed to set
+     */
+    public void setPersonOwed(Participant personOwed) {
+        this.personOwed = personOwed;
     }
 
+    /**
+     * sets the person that is in debt
+     * @param personInDebt person in debt to set
+     */
     public void setPersonInDebt(Participant personInDebt) {
         this.personInDebt = personInDebt;
     }
 
-    public void setPayedOf(boolean payedOf) {
-        this.payedOf = payedOf;
+    /**
+     * sets boolean was the debt paid off
+     * @param paidOff boolean value to set
+     */
+    public void setPaidOff(boolean paidOff) {
+        this.paidOff = paidOff;
     }
 
+    /**
+     * sets the date when the debt was created
+     * @param dateCreation date to set
+     */
     public void setDateCreation(Date dateCreation) {
         this.dateCreation = dateCreation;
     }
 
+    /**
+     * sets the expanse that the debt is for
+     * @param debtFor expense to set
+     */
     public void setDebtFor(Expense debtFor) {
         this.debtFor = debtFor;
     }
 
+    /**
+     * checks if two debts are equal
+     * @param other the object to compare to
+     * @return boolean are the objects equal
+     */
     @Override
     public boolean equals(Object other) {
         if (this == other) return true;
         if (other == null || getClass() != other.getClass()) return false;
         Debt debt = (Debt) other;
-        return amount == debt.amount && payedOf == debt.payedOf && Objects.equals(id, debt.id)
-                && Objects.equals(personOwned, debt.personOwned) && Objects.equals(personInDebt, debt.personInDebt)
+        return amount == debt.amount && paidOff == debt.paidOff && Objects.equals(id, debt.id)
+                && Objects.equals(personOwed, debt.personOwed) && Objects.equals(personInDebt, debt.personInDebt)
                 && Objects.equals(dateCreation, debt.dateCreation) && Objects.equals(debtFor, debt.debtFor);
     }
 
+    /**
+     * generates a hashcode for the debt
+     * @return the hashcode for the debt
+     */
     @Override
     public int hashCode() {
-        return Objects.hash(id, amount, personOwned, personInDebt, payedOf, dateCreation, debtFor);
+        return Objects.hash(id, amount, personOwed, personInDebt, paidOff, dateCreation, debtFor);
     }
 
+    /**
+     * creates a string version of the debt
+     * @return the string version of the debt
+     */
     @Override
     public String toString() {
         return "Debt{" +
                 "id: " + id +
                 ", amount owned:" + amount +
-                ", to " + personOwned +
+                ", to " + personOwed +
                 ", by" + personInDebt +
-                ", payedOf: " + payedOf +
+                ", payedOf: " + paidOff +
                 ", debt created on: " + dateCreation +
                 ", debt for=" + debtFor +
                 '}';
-    }// 73 + 19
+    }
 }
