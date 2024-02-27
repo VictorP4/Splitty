07c6765286eb78098809d2dfb1c436c2f0d4c204
Egemen Yildiz <E.yildiz-2@student.tudelft.nl>
2024-02-27 21:23:42 +0100

    Removing unused imports for checkstyle

--- commons/src/test/java/commons/ParticipantTest.java
+++ commons/src/test/java/commons/ParticipantTest.java
@@ -2,9 +2,6 @@ package commons;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParticipantTest {
