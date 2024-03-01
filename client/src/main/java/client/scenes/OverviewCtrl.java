5c6f9342fae373c2038968b2f66b982f2f2b411d
Robin Taekema <r.d.taekema@student.tudelft.nl>
2024-03-01 16:50:45 +0100

    removed unused imports

--- client/src/main/java/client/scenes/OverviewCtrl.java
+++ client/src/main/java/client/scenes/OverviewCtrl.java
@@ -9,8 +9,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+
 /**
  * Controller class for the overview scene.
  */
