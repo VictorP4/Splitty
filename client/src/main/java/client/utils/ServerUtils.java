724272752b69b7de71fb40273404d9de630acedd
Lara Glamuzina <l.glamuzina@student.tudelft.nl>
2024-03-18 23:39:29 +0100

    fixed path in delete & update expense, was missing /

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -303,7 +303,7 @@ public class ServerUtils {
 	 */
 	public Response deleteExpense(Long id, Expense expense) {
 		return ClientBuilder.newClient(new ClientConfig())
-				.target(server).path("api/events/" + id + "/expenses" + expense.getId())
+				.target(server).path("api/events/" + id + "/expenses/" + expense.getId())
 				.request(APPLICATION_JSON)
 				.accept(APPLICATION_JSON)
 				.delete();
@@ -318,7 +318,7 @@ public class ServerUtils {
 	 */
 	public Expense updateExpense(Long id, Expense expense) {
 		return ClientBuilder.newClient(new ClientConfig())
-				.target(server).path("api/events/" + id + "/expenses" + expense.getId())
+				.target(server).path("api/events/" + id + "/expenses/" + expense.getId())
 				.request(APPLICATION_JSON)
 				.accept(APPLICATION_JSON)
 				.put(Entity.entity(expense, APPLICATION_JSON), Expense.class);
