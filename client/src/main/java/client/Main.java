d0e16321d462d0844754b40b3a5b3edf4258334c
Robin Taekema <r.d.taekema@student.tudelft.nl>
2024-03-28 00:20:49 +0100

    accidentally removed a line. added it back right now

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -71,7 +71,9 @@ public class Main extends Application {
         mainCtrl.initialize(primaryStage, addExpense, contactDetails,
                 invitation, openDebts, statistics, startScreen, eventOverview, adminEventOverview, addTag,
                 settingsPage);
-
+        primaryStage.setOnCloseRequest((e)->{
+            adminEventOverview.getKey().stop();
+        });
     }
 
     /**
