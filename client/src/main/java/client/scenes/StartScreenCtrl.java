2f26db2383fa8589e9ee89f1eb14b40cebbe242f
skutsarov <S.Z.Kutsarov@student.tudelft.nl>
2024-04-05 18:26:04 +0200

    jdoc

--- client/src/main/java/client/scenes/StartScreenCtrl.java
+++ client/src/main/java/client/scenes/StartScreenCtrl.java
@@ -12,7 +12,6 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
