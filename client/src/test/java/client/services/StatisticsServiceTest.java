79505fc3ed68406880f60293e2adc3a1c05652b2
Robin Taekema <r.d.taekema@student.tudelft.nl>
2024-04-11 19:41:01 +0200

    Adjusted a method to also enter the else-clause & added 2 tests to create 100% line coverage

--- client/src/test/java/client/services/StatisticsServiceTest.java
+++ client/src/test/java/client/services/StatisticsServiceTest.java
@@ -81,16 +81,37 @@ public class StatisticsServiceTest {
     void testPopulateTagColors() {
         List<Expense> expenses = new ArrayList<>();
         expenses.add(new Expense("Expense 1", 100.0, null, null, null, new Tag("Food", 255, 0, 0), "USD"));
-        expenses.add(new Expense("Expense 2", 50.0, null, null, null, new Tag("Travel", 0, 255, 0), "USD"));
+        expenses.add(new Expense("Expense 2", 50.0, null, null, null, null, "USD"));
         Event event1 = new Event();
 
         for (Expense e: expenses) {
             event1.getExpenses().add(e);
         }
 
-        Map<String, Color> expected = Map.of("Food", Color.rgb(255, 0, 0), "Travel", Color.rgb(0, 255, 0));
+        Map<String, Color> expected = Map.of("Food", Color.rgb(255, 0, 0), "Other", Color.rgb(255, 255, 255));
         Map<String, Color> actual = statisticsService.populateTagColors(event1);
 
         assertEquals(expected, actual);
+    }
+
+    /**
+     * Tests the formatting of the getDataAmount
+     */
+    @Test
+    void testGetDataAmount() {
+        String tagName = "test";
+        double amount = 150.0;
+        String expected = "test\n150.00 ";
+        assertEquals(expected, statisticsService.getDataAmount(tagName, amount));
+    }
+
+    /**
+     * Tests the formatting of the getDataRelative
+     */
+    @Test
+    void testGetDataRelative() {
+        double relativeValue = 150.00;
+        String expected = "(15000.00%)";
+        assertEquals(expected, statisticsService.getDataRelative(relativeValue));
     }
 }
